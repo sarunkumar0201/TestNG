5cbbeb786267a245d2120c65e250dc7693f68abc
sarunkumar0201 <arun5050@gmail.com>
2020-09-17 15:39:04 +0530

    today1709

--- src/main/java/org/TestNGNew/TestNG1.java
+++ src/main/java/org/TestNGNew/TestNG1.java
@@ -1,5 +1,11 @@
 package org.TestNGNew;
 
-public class TestNG1 {
+public class TestNG1 
 
+{	
+	public static void main(String[] args) 
+	{
+		System.out.println("Hai");
+		System.out.println("Hello");
+	}
 }
